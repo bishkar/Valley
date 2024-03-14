1928b2c1d42b12b863135ec7565b0f9619fc39f0
bishkar <mvolodymyr751@gmail.com>
2024-03-14 20:02:39 +0200

    added tokens to response

--- backend/user/views.py
+++ backend/user/views.py
@@ -14,3 +14,18 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+    
+        user = User.objects.get(email=request.data['email'])
+        token = MyTokenObtainPairSerializer.get_token(user)
+
+        response.data = {
+            "access": str(token.access_token),
+            "refresh": str(token),
+            "first_name": request.data['first_name'],
+            "last_name": request.data['last_name'],
+            "email": request.data['email']
+        }
+        return response
