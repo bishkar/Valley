3b089d1750ac5cd17b66ce33da17664aaca11e29
imaksb <m.bychyniuk@gmail.com>
2024-03-25 19:34:36 +0200

    articles permission

--- backend/article/permissions.py
+++ backend/article/permissions.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAccountAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
+        self.view.throttle_scope = 'anon'
         return bool(
             request.method in SAFE_METHODS or
             request.user and
