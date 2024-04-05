750ae510fce28352dd2333c111a1f4356f525e05
bishkar <mvolodymyr751@gmail.com>
2024-04-05 21:04:18 +0300

    fix favourites

--- backend/article/serializers.py
+++ backend/article/serializers.py
@@ -10,13 +10,13 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ['pk', 'original_title', 'translated_title', 'original_content', 'translated_content',
-                  'link_to_product', 'created_at', 'image_urls', 'images', 'tags']
+                  'link_to_product', 'created_at', 'image_urls', 'images', 'tags', 'tags_name']
         extra_kwargs = {
             'images': {'write_only': True},
             'tags': {'write_only': True}
         }
         write_only_fields = ['images']
-        read_only_fields = ['created_at', 'pk', 'image_urls']
+        read_only_fields = ['created_at', 'pk', 'image_urls', 'tags_name']
 
     @extend_schema_field(serializers.ListField(child=serializers.CharField()))
     def get_image_urls(self, obj):
