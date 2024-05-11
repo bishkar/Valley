beaa244645d8861db6a240f664be7c4f9370f1cf
Den <den204d@gmail.com>
2024-05-11 20:23:04 +0300

    change hover effect

--- src/Components/Category/CategoryItem.jsx
+++ src/Components/Category/CategoryItem.jsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 
 export default function CategoryItem({ category }) {
   const { t } = useTranslation();
-  // const { it_category, en_category } = category;
+
   return (
     <>
       <Link to={`/category/${category.pk}`}>
