00f60b23ec3f6e160d8b3a09e388318624a00add
Den <den204d@gmail.com>
2024-05-14 12:42:09 +0300

    file again

--- src/Pages/FavouritePage.jsx
+++
@@ -1,9 +0,0 @@
-import Favourites from "../components/Favourites/Favourites.jsx";
-
-export default function FavouritePage() {
-  return (
-    <div>
-      <Favourites />
-    </div>
-  );
-}
--- src/pages/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./Main";
-export * from "./SearchPage";
-export * from "./ResultPost";
